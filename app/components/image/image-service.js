2d9be2d2027f7ce30b7f3dde51beccdb9e90333b
dsottimano <dsottimano@gmail.com>
2017-02-06 12:29:15 -0700

    apis fixed to https

--- app/components/image/image-service.js
+++ app/components/image/image-service.js
@@ -1,10 +1,10 @@
 function ImageService() {
-	var url = 'http://bcw-getter.herokuapp.com/?url=';
+	var url = 'https://bcw-getter.herokuapp.com/?url=';
 	var url2 = 'http://www.splashbase.co/api/v1/images/random'
 	var apiUrl = url + encodeURIComponent(url2);
 
 	this.getImage = function (cb) {
-		return $.get(apiUrl, { images_only: true}, function (res) {
+		return $.get(apiUrl, {images_only: true}, function (res) {
 			console.log('Image Data:', res)
 			JSON.parse(res)
 			cb(res)
